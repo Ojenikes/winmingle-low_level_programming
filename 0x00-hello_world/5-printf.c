a2cf8eccedacb86a9f2aa4d057afd7e07d420cae
Seyi Ojenike <seyi@Lenovo.localdomain>
2026-05-03 12:18:16 +0100

    print a line of text using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Author: Seyi Ojenike
+ * Programme: WinMingle SE
+ * Description: print using printf
+ * 
+ * return: 0
+ */
+
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
