300d3fb894c310dcd995a1ad74366bf7fd0e3f17
Seyi Ojenike <seyi@Lenovo.localdomain>
2026-05-03 12:07:53 +0100

    a C program that prints exactly a line of text. plus minor additions to some others

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Author: Seyi Ojenike
+ * Programme: WinMingle Software Engineering Programme
+ * Description: winmingle-low_level_programming.
+ * A program that prints in line with 'puts' function
+ * Return 0
+ */
+
+int main(void){
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
