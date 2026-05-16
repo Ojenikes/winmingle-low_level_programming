91038f2c7c4b143ea85ac45b82a5bcd60c024fac
Seyi Ojenike <seyi@Lenovo.localdomain>
2026-05-16 15:53:18 +0100

    Print the lowercase alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Author: Seyi Ojenike
+ * Program: WinMingle Community C Training
+ * Description: Prints lowercase alphabet in reverse order
+ */
+
+int main(void)
+{
+    char letter;
+
+    for (letter = 'z'; letter >= 'a'; letter--)
+    {
+        putchar(letter);
+    }
+
+    putchar('\n');
+
+    return (0);
+}
