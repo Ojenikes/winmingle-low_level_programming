ae5eda65416d6b77b226ed278762950add0cfea7
Seyi Ojenike <seyi@Lenovo.localdomain>
2026-05-16 16:08:37 +0100

    Print all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * Author: Seyi Ojenike
+ * Program: WinMingle Community C Training
+ * Description: Prints all unique two-digit combinations
+ */
+
+int main(void)
+{
+    int i;
+    int j;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
+            putchar('0' + i);
+            putchar('0' + j);
+
+            if (i != 8 || j != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
